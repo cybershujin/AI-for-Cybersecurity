3282ff76c43283a479902bc84d97cabe7330fb0e
Rachel James <162050062+cybershujin@users.noreply.github.com>
2024-05-17 09:31:21 -0700

    Update reverse_word_index_dict.py

--- Scripts/Data Processing and Cleaning/reverse_word_index_dict.py
+++ Scripts/Data Processing and Cleaning/reverse_word_index_dict.py
@@ -1,4 +1,4 @@
-# Part 2, a reverse word index dictionary after you created the dictionary using the aquisition scripts and then reverse sorted it into a dictionary 
+# Part 2, a reverse word index dictionary after you created the dictionary using the aquisition scripts and then reverse sorted it
 
 reverse_word_index = {k:v for v, k in word_index.items()}
 
@@ -18,4 +18,6 @@ def untokenize_email(indices):
     
 print(' '.join(untokenize_email(tokenize_email(get_words('../data/file/path/filename'), words=10000))))
 
-# arbitrarily I am limiting this to 10k words, but you can do whatever you like. this is just trying to optomize for processing time
+# arbitrarily I am limiting this to 10k words, but you can do whatever you like. this is just trying to optomize for processing time and that if this value is too low, it will be a bunch of blanks
+# by tokenizing the words this way, I can use algebra equations and things like linear regression in deep learning neural networks to measure loss and get performance improvements
+#using the untokenize allows me to reverse the number index to see what word it belongs to
