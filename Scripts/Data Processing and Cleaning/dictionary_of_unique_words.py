fb6d5e61ee90dbdc24af31fd44cba8a8bd178d7c
Rachel James <162050062+cybershujin@users.noreply.github.com>
2024-05-17 09:20:42 -0700

    Update dictionary_of_unique_words.py

--- Scripts/Data Processing and Cleaning/dictionary_of_unique_words.py
+++ Scripts/Data Processing and Cleaning/dictionary_of_unique_words.py
@@ -1,5 +1,5 @@
 # this gets the words and then sorts them into a dictionary where it is sorted by frequency and paired with a number that represents the number of times that word appears in the file
-# 
+# more efficent way of doing this is at https://github.com/cybershujin/AI-for-Cybersecurity/blob/main/Scripts/Data%20Processing%20and%20Cleaning/dict_frequency.py
 all_words = dict()
 for file in get_file_list("../data/file/path/filename"):
     words = get_words(file)
