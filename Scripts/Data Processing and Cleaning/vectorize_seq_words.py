19b31559f0cd8d7ec784d6be8d595b5d5d89563d
Rachel James <162050062+cybershujin@users.noreply.github.com>
2024-05-17 09:42:21 -0700

    Update vectorize_seq_words.py

--- Scripts/Data Processing and Cleaning/vectorize_seq_words.py
+++ Scripts/Data Processing and Cleaning/vectorize_seq_words.py
@@ -3,6 +3,8 @@
 # Your function should multi-hot encode the input list of word indices representing emails. Use the dimension value to define the number of features in your multi-hot encoded array
 # Return the multi-hot encoded array
 #note that this example is using the spam / legit email analysis example
+#if you have not imported numpy yet uncomment this next line
+#import numpy as np
   
 def vectorize_sequence(word_index_array, dimension=WordsToWorkWith):
     """
@@ -15,3 +17,16 @@ def vectorize_sequence(word_index_array, dimension=WordsToWorkWith):
     return results
 
 vectorize_sequence(x_spam_data).shape
+
+## afer this, you'll to vectorize this so you have labels
+x_legit_vectorized = vectorize_sequence(x_legit_data)
+x_spam_vectorized = vectorize_sequence(x_spam_data)
+
+#Merge the vectorized ham and spam into a single array. Add a column to this new array that contains a 0 to indicate legit and a 1 to indicate spam.
+
+merged_data = np.append(x_legit_vectorized, x_spam_vectorized, axis=0)
+labels = np.zeros(x_legit_vectorized.shape[0])
+labels = np.append(labels, np.ones(x_spam_vectorized.shape[0]))
+labels = labels.reshape(labels.shape[0],1)
+all_data = np.append(labels, merged_data, axis=1)
+all_data.shape
